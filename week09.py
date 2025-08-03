f633addbfc29a61580766e85887a6fc9944ddc4d
atamayo2 <157082444+atamayo2@users.noreply.github.com>
2025-08-03 21:34:35 +0000

    week09 hw

--- week09.py
+++ week09.py
@@ -50,3 +50,45 @@ def count_occurrences_iterative(data_list, target):
 
 # WRITE YOUR CODE BELOW
 
+# RECURSIVE SUM OF DIGITS
+
+def sum_of_digits(n: int, base: int = 10) -> int:
+    """Computes the sum of digits for a number recursively."""
+    if n < base:
+        result = n
+    else:
+        result = (n % base) + sum_of_digits(n // base, base)
+    return result
+# end method
+
+# Test sum_of_digits
+print("\nTesting sum_of_digits (recursive)")
+print(sum_of_digits(11))   
+print(sum_of_digits(12)) 
+print(sum_of_digits(21))   
+print(sum_of_digits(1))    
+print(sum_of_digits(2))    
+print(sum_of_digits(22))  
+
+
+# RECURSIVE COUNT OCCURRENCES
+
+def count_occurrences(source: list[int], target: int) -> int:
+    """Counts how many times a target value appears in the list recursively."""
+    if source:
+        occurrence = 1 if source[0] == target else 0
+        result = occurrence + count_occurrences(source[1:], target)
+    else:
+        result = 0
+    return result
+# end method count_occurrences
+
+# Test count_occurrences with  list
+my_list = [11, 12, 21, 1, 2, 22]
+
+print("\nTesting count_occurrences (recursive)")
+print(count_occurrences(my_list, 11)) 
+print(count_occurrences(my_list, 2))   
+print(count_occurrences(my_list, 5))   
+print(count_occurrences(my_list, 22))  
+print(count_occurrences(my_list, 99))  
